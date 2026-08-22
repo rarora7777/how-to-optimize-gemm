aaef3ed577a9960ec65d46de904972be0fe172b4
Rahul Arora <rarora7777@gmail.com>
2026-08-21 19:33:20 -0600

    Updated theoretical maximum for M3

--- src/HowToOptimizeGemm/PlotAll.py
+++ src/HowToOptimizeGemm/PlotAll.py
@@ -4,7 +4,7 @@ import pathlib
 
 # Indicate the number of floating point operations that can be executed
 # per clock cycle
-nflops_per_cycle = 4
+nflops_per_cycle = 32
 
 # Indicate the number of processors being used (in case you are using a
 # multicore or SMP)
